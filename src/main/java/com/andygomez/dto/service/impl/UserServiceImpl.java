479082e4fd6f10bf96210376de169fae7ed677b5
Andy Gomez <andygomezmojica@gmail.com>
2023-01-11 18:41:19 -0600

    add dependency mapstruct

--- src/main/java/com/andygomez/dto/service/impl/UserServiceImpl.java
+++ src/main/java/com/andygomez/dto/service/impl/UserServiceImpl.java
@@ -4,10 +4,12 @@ import com.andygomez.dto.exceptions.NotFoundException;
 import com.andygomez.dto.model.User;
 import com.andygomez.dto.repository.UserRepository;
 import com.andygomez.dto.service.UserService;
+import com.andygomez.dto.util.MessageUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Locale;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -15,10 +17,14 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository repository;
 
+    @Autowired
+    private MessageUtil messageUtil;
+
     @Override
     public User findUserById(Long id) {
         return repository.findById(id).orElseThrow(
-                () -> new NotFoundException("User not found")
+                () -> new NotFoundException(messageUtil
+                        .getMessage("notFound",null, Locale.getDefault()))
         );
     }
 
@@ -35,7 +41,8 @@ public class UserServiceImpl implements UserService {
     @Override
     public void updateUser(Long id, User user) {
         User newUser = repository.findById(id).orElseThrow(
-                () -> new NotFoundException("User does not exist")
+                () -> new NotFoundException(messageUtil
+                        .getMessage("notFound", null, Locale.getDefault()))
         );
         newUser.setName(user.getName());
         newUser.setEmail(user.getEmail());
@@ -46,7 +53,8 @@ public class UserServiceImpl implements UserService {
     @Override
     public void deleteUserById(Long id) {
         User user1 = repository.findById(id).orElseThrow(
-                ()-> new NotFoundException("User not found")
+                ()-> new NotFoundException(messageUtil
+                        .getMessage("notFound", null, Locale.getDefault()))
         );
         repository.deleteById(id);
     }
